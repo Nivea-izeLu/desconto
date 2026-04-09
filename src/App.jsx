fed31e93c523a2bd9a38c8461f301e6426d81439
aluno <e023aluno@outlook.com>
2026-04-09 17:34:43 -0300

    subindo arquivos

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Informacoes from "./components/informacoes";
+import Usuario from "./components/usuario";
+
+function App() {
+  return (
+    <div>
+
+      <h1>Exemplo Aula React</h1>
+
+      <Informacoes />
+
+      <hr />
+
+      <Usuario />
+
+    </div>
+  );
+}
+
+export default App;
